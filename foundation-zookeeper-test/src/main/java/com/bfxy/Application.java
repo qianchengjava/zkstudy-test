5ed245bef5fb70ad88725a237fc21581771433ed
qiancheng <qiancheng@shuidihuzhu.com>
2020-04-25 19:37:03 +0800

    bbb

--- foundation-zookeeper-test/src/main/java/com/bfxy/Application.java
+++ foundation-zookeeper-test/src/main/java/com/bfxy/Application.java
@@ -2,8 +2,10 @@ package com.bfxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {"com.bfxy"})
 public class Application {
 
 	public static void main(String[] args) {
